0fdc47185ceb893a54976619b0e44f9188c80aff
pharrisenterprises <pharrisenterprises@gmail.com>
2025-09-23 23:28:35 -0400

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 
 export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return <div className={cn("rounded-xl border bg-white p-4 shadow", className)} {...props} />;
