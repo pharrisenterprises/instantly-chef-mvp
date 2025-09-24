19c412a38ea8f98ff29b8d75e6a5a32fcecf7ad1
pharrisenterprises <pharrisenterprises@gmail.com>
2025-09-23 23:28:06 -0400

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { cva } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50",
@@ -9,18 +9,18 @@ const buttonVariants = cva(
       variant: {
         default: "bg-black text-white hover:bg-gray-800",
         outline: "border border-gray-300 bg-white hover:bg-gray-100",
-        ghost: "hover:bg-gray-100",
+        ghost: "hover:bg-gray-100"
       },
       size: {
         sm: "h-8 px-3",
         default: "h-10 px-4",
-        lg: "h-12 px-6 text-base",
-      },
+        lg: "h-12 px-6 text-base"
+      }
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
-    },
+      size: "default"
+    }
   }
 );
 
